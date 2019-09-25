dca002efe90bea8474bd4390542105ddc7d32ee6
Javier Pedroza <javierlpedroza@qdata.io>
2019-09-25 14:29:45 -0500

    upload project

---
+++ products.js
@@ -0,0 +1,18 @@
+[
+    {    
+        "name": 'Televisor',
+        'value': 'tv'          
+    },
+    {    
+        "name": 'Colchon',
+        'value': 'colchon'          
+    },
+    {    
+        "name": 'Telefono',
+        'value': 'tv'          
+    },
+    {    
+        "name": 'Computador',
+        'value': 'pc'          
+    }
+]
